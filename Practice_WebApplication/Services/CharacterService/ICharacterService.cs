73ea9b740e4629059392325f534634393cbddfc4
Pedro <plguzman1993@gmail.com>
2023-07-31 08:30:12 -0700

    16. Character Service Completed

---
+++ Practice_WebApplication/Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice_WebApplication.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+
+        Character GetCharacterById(int id);
+
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
