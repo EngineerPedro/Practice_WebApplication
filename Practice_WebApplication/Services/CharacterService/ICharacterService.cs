883723f9692935f36c4438c0598d6b1a87667f96
Pedro <plguzman1993@gmail.com>
2023-07-31 08:44:43 -0700

    18. Asynchronous Calls Completd

--- Practice_WebApplication/Services/CharacterService/ICharacterService.cs
+++ Practice_WebApplication/Services/CharacterService/ICharacterService.cs
@@ -2,10 +2,10 @@
 {
     public interface ICharacterService
     {
-        List<Character> GetAllCharacters();
+        Task<List<Character>> GetAllCharacters();
 
-        Character GetCharacterById(int id);
+        Task<Character> GetCharacterById(int id);
 
-        List<Character> AddCharacter(Character newCharacter);
+        Task<List<Character>> AddCharacter(Character newCharacter);
     }
 }
